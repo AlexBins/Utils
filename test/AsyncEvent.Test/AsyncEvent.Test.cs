26df350c74dbb2dde6964bf4241482698a58ee40
Sascha Binsmaier <alexander.binsmaier@bayoo.net>
2022-06-27 11:45:48 +0200

    Added initial project files.
    Added AsyncEvent implementation.

---
+++ test/AsyncEvent.Test/AsyncEvent.Test.cs
@@ -0,0 +1,18 @@
+namespace AsyncEvent.Test
+{
+    using NUnit.Framework;
+
+    public class AsyncEventTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
